a14c9b79abf30a15bfce0112747e70353b9f5791
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2022-12-13 12:56:22 +0100

    Solved Day 9

--- app/src/main/kotlin/day09/Day09.kt
+++ app/src/main/kotlin/day09/Day09.kt
@@ -3,6 +3,8 @@ package day09
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import util.splitIntoPair
+import kotlin.math.abs
 
 fun main(args: Array<String>) {
     val day = 9
@@ -11,10 +13,130 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay09Part1, ::solveDay09Part2)
 }
 
+data class Pos(val x: Int, val y: Int) {
+    fun move(direction: String): Pos {
+        when (direction) {
+            "U" -> {
+                return Pos(x, y + 1)
+            }
+
+            "D" -> {
+                return Pos(x, y - 1)
+            }
+
+            "L" -> {
+                return Pos(x - 1, y)
+            }
+
+            "R" -> {
+                return Pos(x + 1, y)
+            }
+        }
+        throw IllegalArgumentException("Invalid direction")
+    }
+
+    fun follow(head: Pos): Pos {
+
+        val xDiff = head.x - x
+        val yDiff = head.y - y
+
+        if (abs(xDiff) > 1 && abs(yDiff) >= 1 || abs(xDiff) >= 1 && abs(yDiff) > 1) {
+            return Pos(this.x + 1 * sign(xDiff), this.y + 1 * sign(yDiff))
+        } else if (abs(xDiff) > 1) {
+            return Pos(this.x + 1 * sign(xDiff), this.y)
+        } else if (abs(yDiff) > 1) {
+            return Pos(this.x, this.y + +1 * sign(yDiff))
+        }
+        return this
+    }
+}
+
+fun sign(value: Int): Int {
+    if (value < 0) {
+        return -1
+    } else if (value > 0) {
+        return 1
+    } else {
+        return 0
+    }
+}
+
 fun solveDay09Part1(input: List<String>): Int {
-    TODO()
+    val moves = input.map { it.splitIntoPair(" ") }
+
+    var head = Pos(0, 0)
+    var tail = Pos(0, 0)
+    val tailVisited = mutableSetOf(tail)
+    moves.forEach { (dir, repeat) ->
+        repeat(repeat.toInt()) {
+//            printGrid(5, head, listOf(tail));
+
+            val head_new = head.move(dir)
+            val tail_new = tail.follow(head_new)
+            println("head: $head")
+            println("tail: $tail")
+
+            tailVisited.add(tail_new)
+            head = head_new
+            tail = tail_new
+            println()
+        }
+
+    }
+
+    return tailVisited.size
+
+}
+
+fun printGrid(n: Int, head: Pos, tails: List<Pos>) {
+
+    for (y in n downTo 0) {
+        for (x in 0..n) {
+            if (x == head.x && y == head.y) {
+                print("H")
+            } else if (tails.firstOrNull { it.x == x && it.y == y } != null) {
+                print(tails.indexOf(tails.firstOrNull { it.x == x && it.y == y }))
+//                print("T")
+            } else {
+                print(".")
+            }
+        }
+        println()
+    }
 }
 
 fun solveDay09Part2(input: List<String>): Int {
-    TODO()
+    val moves = input.map { it.splitIntoPair(" ") }
+
+    var head = Pos(0, 0)
+    val tails = mutableListOf(
+        Pos(0, 0),
+        Pos(0, 0),
+        Pos(0, 0),
+        Pos(0, 0),
+        Pos(0, 0),
+        Pos(0, 0),
+        Pos(0, 0),
+        Pos(0, 0),
+        Pos(0, 0),
+    )
+    val tailVisited = tails.toMutableSet()
+    moves.forEach { (dir, repeat) ->
+        repeat(repeat.toInt()) {
+            printGrid(5, head, tails)
+
+            val head_new = head.move(dir)
+            for ((index, tail) in tails.withIndex()) {
+                val currentFollowHead = if (index > 0) tails[index - 1] else head_new
+                tails[index] = tail.follow(currentFollowHead)
+            }
+
+            tailVisited.add(tails[8])
+            head = head_new
+//            println()
+        }
+
+    }
+
+    return tailVisited.size
 }
--- app/src/test/kotlin/day09/Day09Test.kt
+++ app/src/test/kotlin/day09/Day09Test.kt
@@ -1,23 +1,24 @@
 package day09
 
-import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-@Ignored
 class Day09Test : FreeSpec({
 
     val sampleInput: List<String> = """
-        2199943210
-        3987894921
-        9856789892
-        8767896789
-        9899965678
+        R 4
+        U 4
+        L 3
+        D 1
+        R 4
+        D 1
+        L 5
+        R 2
     """.trimIndent().split("\n")
 
-    val sampleSolutionPart1 = 15
+    val sampleSolutionPart1 = 13
 
-    val sampleSolutionPart2 = 1134
+    val sampleSolutionPart2 = 1
 
     "Solving day 9" - {
         "part 1 for the sample input should return the correct output" {
