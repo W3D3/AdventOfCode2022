a9519bcb64ab329d348e566f1e7f20d86f280281
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2022-12-13 17:53:52 +0100

    Solved Day 10

--- app/src/main/kotlin/day10/Day10.kt
+++ app/src/main/kotlin/day10/Day10.kt
@@ -3,6 +3,13 @@ package day10
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import util.splitIntoPair
+
+data class AddExecution(var cycles: Int, val arg: Int) {
+    fun cycle() {
+        cycles--
+    }
+}
 
 fun main(args: Array<String>) {
     val day = 10
@@ -12,9 +19,91 @@ fun main(args: Array<String>) {
 }
 
 fun solveDay10Part1(input: List<String>): Int {
-    TODO()
+    var x = 1
+    var cycle = 1
+    val executionList = mutableListOf<AddExecution>()
+
+    var sum = 0
+
+    val cmds = input.map { it.splitIntoPair(" ") }.toMutableList()
+
+
+    var i = 0
+    while (executionList.filter { it.cycles >= 0 }.isNotEmpty() || i < cmds.size) {
+
+        sum += signalStrengthOnCorrectCyle(x, cycle)
+
+        if (i < cmds.size) {
+            val (cmd, arg) = cmds[i]
+            if (cmd == "addx") {
+                executionList.add(AddExecution(1, arg.toInt()))
+                cmds.add(i + 1, "fakenoop" to "")
+            }
+        }
+
+        val toExecute = executionList.filter { it.cycles == 0 }
+        toExecute.forEach { x += it.arg }
+
+        executionList.forEach { it.cycle() }
+        cycle++
+        i++
+
+    }
+
+    println("$cycle | X: $x |")
+    return sum
+}
+
+
+fun signalStrengthOnCorrectCyle(x: Int, cycle: Int): Int {
+    println("$cycle | X: $x |")
+    if (cycle == 20 || (cycle - 20) % 40 == 0) {
+        println("!!!! $cycle | X: $x |")
+        return x * cycle
+    }
+    return 0
 }
 
 fun solveDay10Part2(input: List<String>): Long {
-    TODO()
+    var x = 1
+    var cycle = 1
+    val executionList = mutableListOf<AddExecution>()
+
+    val cmds = input.map { it.splitIntoPair(" ") }.toMutableList()
+
+    var i = 0
+    while (i < 6 * 40) {
+
+        if (i < cmds.size) {
+            val (cmd, arg) = cmds[i]
+            if (cmd == "addx") {
+                executionList.add(AddExecution(1, arg.toInt()))
+                cmds.add(i + 1, "fakenoop" to "")
+            }
+        }
+
+        // CRT code here
+        crtDraw(x, i)
+
+        val toExecute = executionList.filter { it.cycles == 0 }
+        toExecute.forEach { x += it.arg }
+
+        executionList.forEach { it.cycle() }
+        cycle++
+        i++
+
+    }
+
+    return 0
+}
+
+fun crtDraw(x: Int, cycle: Int) {
+    if (cycle % 40 == 0) {
+        println()
+    }
+    if (setOf(x - 1, x, x + 1).contains(cycle % 40)) {
+        print("█")
+    } else {
+        print(" ")
+    }
 }
--- app/src/test/kotlin/day10/Day10Test.kt
+++ app/src/test/kotlin/day10/Day10Test.kt
@@ -1,36 +1,178 @@
 package day10
 
-import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-@Ignored
 class Day10Test : FreeSpec({
 
-    val sampleInput: List<String> = """
-        [({(<(())[]>[[{[]{<()<>>
-        [(()[<>])]({[<{<<[]>>(
-        {([(<{}[<>[]}>{[]{[(<()>
-        (((({<>}<{<{<>}{[]{[]{}
-        [[<[([]))<([[{}[[()]]]
-        [{[{({}]{}}([{[{{{}}([]
-        {<[[]]>}<{[{[{[]{()[[[]
-        [<(<(<(<{}))><([]([]()
-        <{([([[(<>()){}]>(<<{{
-        <{([{{}}[<[[[<>{}]]]>[]]
+    val simpleInput: List<String> = """
+    noop
+    addx 3
+    addx -5
     """.trimIndent().split("\n")
 
-    val sampleSolutionPart1 = 26397
+    val sampleInput: List<String> = """
+    addx 15
+    addx -11
+    addx 6
+    addx -3
+    addx 5
+    addx -1
+    addx -8
+    addx 13
+    addx 4
+    noop
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx -35
+    addx 1
+    addx 24
+    addx -19
+    addx 1
+    addx 16
+    addx -11
+    noop
+    noop
+    addx 21
+    addx -15
+    noop
+    noop
+    addx -3
+    addx 9
+    addx 1
+    addx -3
+    addx 8
+    addx 1
+    addx 5
+    noop
+    noop
+    noop
+    noop
+    noop
+    addx -36
+    noop
+    addx 1
+    addx 7
+    noop
+    noop
+    noop
+    addx 2
+    addx 6
+    noop
+    noop
+    noop
+    noop
+    noop
+    addx 1
+    noop
+    noop
+    addx 7
+    addx 1
+    noop
+    addx -13
+    addx 13
+    addx 7
+    noop
+    addx 1
+    addx -33
+    noop
+    noop
+    noop
+    addx 2
+    noop
+    noop
+    noop
+    addx 8
+    noop
+    addx -1
+    addx 2
+    addx 1
+    noop
+    addx 17
+    addx -9
+    addx 1
+    addx 1
+    addx -3
+    addx 11
+    noop
+    noop
+    addx 1
+    noop
+    addx 1
+    noop
+    noop
+    addx -13
+    addx -19
+    addx 1
+    addx 3
+    addx 26
+    addx -30
+    addx 12
+    addx -1
+    addx 3
+    addx 1
+    noop
+    noop
+    noop
+    addx -9
+    addx 18
+    addx 1
+    addx 2
+    noop
+    noop
+    addx 9
+    noop
+    noop
+    noop
+    addx -1
+    addx 2
+    addx -37
+    addx 1
+    addx 3
+    noop
+    addx 15
+    addx -21
+    addx 22
+    addx -6
+    addx 1
+    noop
+    addx 2
+    addx 1
+    noop
+    addx -10
+    noop
+    noop
+    addx 20
+    addx 1
+    addx 2
+    addx 2
+    addx -6
+    addx -11
+    noop
+    noop
+    noop
+    """.trimIndent().split("\n")
 
-    val sampleSolutionPart2 = 288957
+    val sampleSolutionPart1 = 13140
 
     "Solving day 10" - {
+        "part 1 for the simple input should return the correct output" {
+            solveDay10Part1(simpleInput) shouldBe 0
+        }
+
         "part 1 for the sample input should return the correct output" {
             solveDay10Part1(sampleInput) shouldBe sampleSolutionPart1
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay10Part2(sampleInput) shouldBe sampleSolutionPart2
+            solveDay10Part2(sampleInput) shouldBe 0
         }
     }
 })
