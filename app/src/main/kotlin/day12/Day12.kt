75148423d7da073271cc6d9ebd2e6a9401a1b7a8
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2022-12-14 16:31:18 +0100

    Solved Day 12

--- app/src/main/kotlin/day12/Day12.kt
+++ app/src/main/kotlin/day12/Day12.kt
@@ -3,7 +3,17 @@ package day12
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import org.jetbrains.kotlinx.multik.api.mk
+import org.jetbrains.kotlinx.multik.api.ndarray
+import org.jetbrains.kotlinx.multik.ndarray.data.D2Array
+import org.jetbrains.kotlinx.multik.ndarray.data.get
+import org.jetbrains.kotlinx.multik.ndarray.operations.forEachMultiIndexed
+import util.Coord
+import util.graphs.NoSuchPathException
+import util.graphs.directed.weighted.DWGraph
+import util.graphs.directed.weighted.Dijkstra
 
+private const val ALPHABET_LENGTH = 26
 
 fun main(args: Array<String>) {
     val day = 12
@@ -12,11 +22,77 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay12Part1, ::solveDay12Part2)
 }
 
-
 fun solveDay12Part1(input: List<String>): Int {
-    TODO()
+    val flatMapChars = input.flatMap { it.toCharArray().toList() }
+    val startIndex = flatMapChars.indexOf('S')
+    val endIndex = flatMapChars.indexOf('E')
+
+    val nestedList = parseInput(input)
+    val mountain = mk.ndarray(nestedList)
+    val graph = createGraph(mountain)
+
+    val dijkstra = Dijkstra(graph, startIndex)
+    val pathTo = dijkstra.pathTo(endIndex)
+
+    return pathTo.count()
 }
 
 fun solveDay12Part2(input: List<String>): Int {
-    TODO()
+    val flatMapChars = input.flatMap { it.toCharArray().toList() }
+    val startIndices = flatMapChars.mapIndexedNotNull { index, c -> if (c == 'a') index else null }
+    val endIndex = flatMapChars.indexOf('E')
+
+    val nestedList = parseInput(input)
+    val mountain = mk.ndarray(nestedList)
+    val graph = createGraph(mountain)
+
+    var min = Int.MAX_VALUE
+    for (startIndex in startIndices) {
+        val dijkstra = Dijkstra(graph, startIndex)
+        try {
+            val pathTo = dijkstra.pathTo(endIndex)
+            if (pathTo.count() < min) {
+                min = pathTo.count()
+            }
+        } catch (_: NoSuchPathException) {
+            //NOSONAR Ignore exception
+        }
+    }
+
+    return min
 }
+
+private fun parseInput(input: List<String>): List<List<Int>> {
+    return input.map { line ->
+        line.toCharArray().map { if (it == 'S') 'a' else if (it == 'E') 'z' else it }.map { it.code - 97 }.toList()
+    }
+}
+
+private fun createGraph(mountain: D2Array<Int>): DWGraph {
+    val graph = DWGraph(mountain.size)
+
+    mountain.forEachMultiIndexed { index, el ->
+        println("> Checking  ${index.toList()} $el")
+        val currentCoord = Coord(index[0], index[1])
+        val directNeighborCoords = currentCoord.getDirectNeighbors(mountain.shape[0], mountain.shape[1])
+        println(directNeighborCoords)
+
+        for ((x, y) in directNeighborCoords) {
+            val directNeighbor = mountain[x, y]
+
+            if ((directNeighbor - el) % ALPHABET_LENGTH <= 1) {
+                // allowed edge
+                graph.addEdge(
+                    multiIndexToFlatIndex(currentCoord.x, currentCoord.y, mountain.shape),
+                    multiIndexToFlatIndex(x, y, mountain.shape),
+                    1.0
+                )
+            }
+        }
+    }
+    return graph
+}
+
+private fun multiIndexToFlatIndex(
+    x: Int, y: Int, shape: IntArray
+) = shape[1] * x + y
--- app/src/test/kotlin/day12/Day12Test.kt
+++ app/src/test/kotlin/day12/Day12Test.kt
@@ -1,83 +1,28 @@
 package day12
 
-import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-@Ignored
 class Day12Test : FreeSpec({
 
-val smallSampleInput: List<String> = """
-    start-A
-    start-b
-    A-c
-    A-b
-    b-d
-    A-end
-    b-end
+val sampleInput: List<String> = """
+    Sabqponm
+    abcryxxl
+    accszExk
+    acctuvwj
+    abdefghi
     """.trimIndent().split("\n")
 
-    val mediumSizeInput: List<String> = """
-    dc-end
-    HN-start
-    start-kj
-    dc-start
-    dc-HN
-    LN-dc
-    HN-end
-    kj-sa
-    kj-HN
-    kj-dc
-    """.trimIndent().split("\n")
-
-val largeInput: List<String> = """
-    fs-end
-    he-DX
-    fs-he
-    start-DX
-    pj-DX
-    end-zg
-    zg-sl
-    zg-pj
-    pj-he
-    RW-he
-    fs-DX
-    pj-RW
-    zg-RW
-    start-pj
-    he-WI
-    zg-he
-    pj-fs
-    start-RW
-    """.trimIndent().split("\n")
-
-    val smallSampleSolutionPart1 = 10
-    val mediumSampleSolutionPart1 = 19
-    val largeSampleSolutionPart1 = 226
-
-    val smallSampleSolutionPart2 = 36
-    val mediumSampleSolutionPart2 = 103
-    val largeSampleSolutionPart2 = 3509
+    val sampleSolutionPart1 = 31
+    val sampleSolutionPart2 = 29
 
     "Solving day 12" - {
-        "part 1 for the small sample input should return the correct output" {
-            solveDay12Part1(smallSampleInput) shouldBe smallSampleSolutionPart1
-        }
-        "part 1 for the medium sample input should return the correct output" {
-            solveDay12Part1(mediumSizeInput) shouldBe mediumSampleSolutionPart1
-        }
-        "part 1 for the large sample input should return the correct output" {
-            solveDay12Part1(largeInput) shouldBe largeSampleSolutionPart1
+        "part 1 for the sample input should return the correct output" {
+            solveDay12Part1(sampleInput) shouldBe sampleSolutionPart1
         }
 
         "part 2 for the small sample input should return the correct output" {
-            solveDay12Part2(smallSampleInput) shouldBe smallSampleSolutionPart2
-        }
-        "part 2 for the medium sample input should return the correct output" {
-            solveDay12Part2(mediumSizeInput) shouldBe mediumSampleSolutionPart2
-        }
-        "part 2 for the large sample input should return the correct output" {
-            solveDay12Part2(largeInput) shouldBe largeSampleSolutionPart2
+            solveDay12Part2(sampleInput) shouldBe sampleSolutionPart2
         }
     }
 })
