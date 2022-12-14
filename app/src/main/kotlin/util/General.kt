f56dbe9987f1a67864a15b9072257b7cf111443a
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2022-12-14 16:21:56 +0100

    Added Algorithms and Data Structures from the Kotlin Algorithm Club

--- app/src/main/kotlin/util/General.kt
+++ app/src/main/kotlin/util/General.kt
@@ -5,3 +5,22 @@ fun String.splitIntoPair(vararg delimiters: String, ignoreCase: Boolean = false)
     val second = if (split.size < 2) "" else split[1]
     return split[0] to second
 }
+
+data class Coord(val x: Int, val y: Int) {
+    fun getDirectNeighbors(dimX: Int, dimY: Int): Collection<Coord> {
+        val neighbors = mutableListOf<Coord>()
+        if (x > 0) {
+            neighbors.add(Coord(x - 1, y))
+        }
+        if (x < dimX - 1) {
+            neighbors.add(Coord(x + 1, y))
+        }
+        if (y > 0) {
+            neighbors.add(Coord(x, y - 1))
+        }
+        if (y < dimY - 1) {
+            neighbors.add(Coord(x, y + 1))
+        }
+        return neighbors;
+    }
+}
