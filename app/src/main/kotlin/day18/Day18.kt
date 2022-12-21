746a4d10c65c14c9843dd4a5586a23da1eb3bda4
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2022-12-21 15:46:10 +0100

    Solved Day 18

--- app/src/main/kotlin/day18/Day18.kt
+++ app/src/main/kotlin/day18/Day18.kt
@@ -3,6 +3,7 @@ package day18
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import util.Pos3D
 
 fun main(args: Array<String>) {
     val day = 18
@@ -11,11 +12,58 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay18Part1, ::solveDay18Part2)
 }
 
-
 fun solveDay18Part1(input: List<String>): Int {
-    TODO()
+    val cubes = input.map { line ->
+        line.split(",").let { Cube(Pos3D(it[0].toLong(), it[1].toLong(), it[2].toLong())) }
+    }
+
+    return cubes.sumOf { it.countExposedSides((cubes - it).toSet()) }
 }
 
 fun solveDay18Part2(input: List<String>): Int {
-    TODO()
+    val cubes = input.map { line ->
+        line.split(",").let { Cube(Pos3D(it[0].toLong(), it[1].toLong(), it[2].toLong())) }
+    }
+
+    val visitedPositions = floodFill(cubes.toSet())
+
+    return cubes.sumOf { it.countTouchingSidesPos(visitedPositions) }
+}
+
+fun floodFill(cubes: Set<Cube>): Set<Pos3D> {
+    val xRange = cubes.minOf { it.pos.x } - 1..cubes.maxOf { it.pos.x } + 1
+    val yRange = cubes.minOf { it.pos.y } - 1..cubes.maxOf { it.pos.y } + 1
+    val zRange = cubes.minOf { it.pos.z } - 1..cubes.maxOf { it.pos.z } + 1
+
+    val startPos = Pos3D(xRange.first, yRange.first, zRange.first)
+    val visitedPositions = mutableSetOf(startPos)
+
+    var basePositions = setOf(startPos)
+    val exploredPositions: MutableList<Pos3D> = mutableListOf()
+    do {
+        for (basePos in basePositions) {
+            exploredPositions += basePos.directNeighbors
+                .filter { it.x in xRange && it.y in yRange && it.z in zRange }
+                .filter { floodPos -> floodPos !in cubes.map { it.pos } }
+                .toSet()
+        }
+        basePositions = (exploredPositions - visitedPositions).toSet()
+        visitedPositions.addAll(basePositions)
+    } while (basePositions.isNotEmpty())
+
+    return visitedPositions
+}
+
+data class Cube(val pos: Pos3D) {
+    fun countExposedSides(others: Set<Cube>): Int {
+        return 6 - countTouchingSides(others)
+    }
+
+    fun countTouchingSides(others: Set<Cube>): Int {
+        return countTouchingSidesPos(others.map { it.pos }.toSet())
+    }
+
+    fun countTouchingSidesPos(others: Set<Pos3D>): Int {
+        return others.count { otherPos -> this.pos.isDirectNeighbor(otherPos) }
+    }
 }
--- app/src/test/kotlin/day18/Day18Test.kt
+++ app/src/test/kotlin/day18/Day18Test.kt
@@ -1,18 +1,29 @@
 package day18
 
-import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-@Ignored
 class Day18Test : FreeSpec({
 
-    val sampleInput: List<String> = listOf(
-    )
+    val sampleInput: List<String> = """
+        2,2,2
+        1,2,2
+        3,2,2
+        2,1,2
+        2,3,2
+        2,2,1
+        2,2,3
+        2,2,4
+        2,2,6
+        1,2,5
+        3,2,5
+        2,1,5
+        2,3,5
+    """.trimIndent().split("\n")
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1 = 64
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2 = 58
 
     "Solving day 18" - {
         "part 1 for the sample input should return the correct output" {
